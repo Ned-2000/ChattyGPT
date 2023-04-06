9b7a28d3026b32014a4cdd9ebaba15970cd04153
Ned Ean <31110452+Ned-2000@users.noreply.github.com>
2023-04-05 20:22:02 -0400

    10s Cooldown implemented

--- cogs/image_generation.py
+++ cogs/image_generation.py
@@ -1,5 +1,6 @@
 import discord, openai, asyncio
 from discord.ext import commands
+from discord.ext.commands import BucketType, cooldown
 
 class Img(commands.Cog):
     """ Image generation using OpenAI, returns link to prompt """
@@ -12,6 +13,7 @@ class Img(commands.Cog):
         print('image_generation.py online.')
     
     @commands.command()
+    @cooldown(1, 10, BucketType.user)
     async def image(self, ctx):
         if ctx.message.content.replace("!image", "") == "":
             await ctx.reply("The '!image' command is used for image generation.")
@@ -23,11 +25,12 @@ class Img(commands.Cog):
         response_message = await ctx.reply("Generating image...")
         
         try:
-            response = openai.Image.create(
-            prompt=ctx.message.content.replace("!image", ""),
-            n=1,
-            size="1024x1024"
-            )
+            async with aiohttp.ClientSession() as session:
+                response = openai.Image.create(
+                prompt=ctx.message.content.replace("!image", ""),
+                n=1,
+                size="1024x1024"
+                )
             image_url = response['data'][0]['url']
             await response_message.edit(content="Here's your generated image link! It will last for one hour before expiry!\n" + str(image_url),delete_after=3600)
             print("\nimage generated:\n\n" + str(image_url))
